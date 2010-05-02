74c66f217fe5bb8c20c15d4cfd2cdcc655ed4d63
xlr8or <xlr8or@xlr8or.com>
2010-05-02 11:21:29 +0200

    bfbc2 config file

---
+++ xlrstats/xlrstats/config/bfbc2.php
@@ -0,0 +1,95 @@
+<?php
+/***************************************************************************
+ * Xlrstats Webmodule
+ * Webfront for XLRstats for B3 (www.bigbrotherbot.com)
+ * (c) 2004-2010 www.xlr8or.com (mailto:xlr8or@xlr8or.com)
+ ***************************************************************************/
+
+/***************************************************************************
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  http://www.gnu.org/copyleft/gpl.html
+ ***************************************************************************/
+
+//*********************
+// These are the standard BFBC2 settings
+//*********************
+
+// Teamnames and colors
+$team1 = "Red"; // red team
+$team2 = "Blue"; // blue team
+$spectators = "Spectators";
+
+
+//*********************
+// Weapons names
+//*********************
+$w[''] = $text["notidentify"];
+
+//*********************
+// Map names
+//*********************
+//Stock
+//$m['Levels/MP_001'] = "Panama Canal";
+//$m['Levels/MP_002'] = "Val Paraiso";
+//$m['Levels/MP_003'] = "Laguna Alta";
+//$m['Levels/MP_004'] = "Isla Inocentes";
+//$m['Levels/MP_005'] = "Atacama Desert";
+//$m['Levels/MP_006'] = "Africa Harbor";
+//$m['Levels/MP_007'] = "White Pass";
+//$m['Levels/MP_008'] = "Nelson Bay";
+$m['Levels/MP_009'] = "Laguna Presa";
+$m['Levels/MP_012'] = "Port Valdez";
+//Conquest Levels
+$m['Levels/MP_001'] = "Panama Canal (Conquest)";
+$m['Levels/MP_003'] = "Laguna Alta (Conquest)";
+$m['Levels/MP_005'] = "Atacama Desert (Conquest)";
+$m['Levels/MP_006CQ'] = "Africa Harbor (Conquest)";
+$m['Levels/MP_007'] = "White Pass (Conquest)";
+$m['Levels/MP_009CQ'] = "Laguna Preza (Conquest)";
+//Rush Levels
+$m['Levels/MP_002'] = "Valparaiso (Rush)";
+$m['Levels/MP_004'] = "Isla Inocentes (Rush)";
+$m['Levels/MP_006'] = "Africa Harbor (Rush)";
+$m['Levels/MP_008'] = "Nelson Bay (Rush)";
+$m['Levels/MP_009GR'] = "Laguna Preza (Rush)";
+$m['Levels/MP_012GR'] = "Port Valdez (Rush)";
+//Squadrush Levels
+$m['Levels/MP_001SR'] = "Panama Canal (Squadrush)";
+$m['Levels/MP_002SR'] = "Valparaiso (Squadrush)";
+$m['Levels/MP_005SR'] = "Atacama Desert (Squadrush)";
+$m['Levels/MP_012SR'] = "Port Valdez (Squadrush)";
+//Squad Deathmatch
+$m['Levels/MP_004SDM'] = "Isla Inocentes (Squad DM)";
+$m['Levels/MP_006SDM'] = "Africa Harbor (Squad DM)";
+$m['Levels/MP_007SDM'] = "White Pass (Squad DM)";
+$m['Levels/MP_009SDM'] = "Laguna Preza (Squad DM)";
+
+$m['None'] = "-Unknown-";
+
+
+//*********************
+// Event names
+//*********************
+$e[''] = "";
+
+
+//*********************
+// Bodypart names
+//*********************
+$b['None'] = $text["noneurt"];
+$b['body'] = $text["notidentify"];
+
+?>

adf37cf9e6473a197061de0f84453f7c06f6c8b2
xlr8or <xlr8or@xlr8or.com>
2011-04-04 21:15:17 +0200

    homefront additions

---
+++ xlrstats/xlrstats/config/homefront.php
@@ -0,0 +1,60 @@
+<?php
+/***************************************************************************
+ * Xlrstats Webmodule
+ * Webfront for XLRstats for B3 (www.bigbrotherbot.com)
+ * (c) 2004-2010 www.xlr8or.com (mailto:xlr8or@xlr8or.com)
+ ***************************************************************************/
+
+/***************************************************************************
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  http://www.gnu.org/copyleft/gpl.html
+ ***************************************************************************/
+
+//*********************
+// These are the standard MOH settings
+//*********************
+
+// Teamnames and colors
+$team1 = "Korea"; // red team
+$team2 = "U.S."; // blue team
+$spectators = "Spectators";
+
+
+//*********************
+// Weapons names
+//*********************
+
+//Rifleman
+$w['[]'] = $text["notidentify"];
+
+//*********************
+// Map names
+//*********************
+
+$m['None'] = "-Unknown-";
+
+//*********************
+// Event names
+//*********************
+$e[''] = "";
+
+//*********************
+// Bodypart names
+//*********************
+$b['None'] = $text["noneurt"];
+$b['body'] = $text["notidentify"];
+
+?>

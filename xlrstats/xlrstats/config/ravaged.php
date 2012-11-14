87797b16dd9caa425e47511d215a461575bf8d2c
Freelander <freelander@fps-gamer.net>
2012-11-14 13:50:53 +0200

    Ravaged: updated config file by Kinsman

--- xlrstats/xlrstats/config/ravaged.php
+++ xlrstats/xlrstats/config/ravaged.php
@@ -22,91 +22,132 @@
  *
  *  http://www.gnu.org/copyleft/gpl.html
  ***************************************************************************/
-
-//*******************************
-// Standard Homefront Settings
-//*******************************
+ 
+//*********************
+// These are the standard Ravaged settings
+//*********************
 
 // Teamnames and colors
 $team1 = "Scavengers"; // red team
 $team2 = "Resistance"; // blue team
 $spectators = "Spectators";
 
-//*********************
-// Weapons
-//*********************
 
 //*********************
-//Assault Rifles
+// Weapons names
 //*********************
 
-$w['R_DmgType_HeliRocket'] = "Heli Rocket";
-$w['R_DmgType_VehicleCollision'] = "Vehicle Collision";
-$w['R_DmgType_RPG'] = "RPG";
-$w['R_DmgType_Mp5'] = "MP5";
-$w['R_DmgType_M16'] = "M16";
-$w['R_DmgType_AK47'] = "AK47";
-$w['R_DmgType_HeavyAssault'] = "Heavy Assault";
-$w['R_DmgType_50cal'] = "50 Cal";
+$w['R_DmgType_AN94'] = "AN-94";
+$w['R_DmgType_AK47'] = "AK-47";
 $w['R_DmgType_Minigun'] = "Minigun";
-$w['R_DmgType_StrykerCannon'] = "Stryker Cannon";
-$w['R_DmgType_TennisBallBomb'] = "Tennis Ball";
-$w['R_DmgType_Grenade_M19'] = "M19";
-$w['R_DmgType_M249'] = "M249";
-$w['R_DmgType_AN94'] = "AN94";
-$w['UTDmgType_VehicleCollision'] = "Vehicle Collision";
-$w['R_DmgType_SawedOffShotgun'] = "Sawed Off Shotgun";
-$w['R_DmgType_Autoshotgun'] = "Auto-shotgun";
-$w['R_DmgType_RocketArtillery'] = "Rocket Artillery";
-$w['R_DmgType_357'] = "357";
-$w['R_DmgType_HeliStinger'] = "Heli Stinger";
+$w['R_DmgType_50cal'] = ".50 Cal Machinegun";
+$w['R_DmgType_Grenade_M79'] = "M79";
+$w['R_DmgType_P99'] = "P99";
+$w['R_DmgType_SniperPrimary'] = "Sniper";
+$w['R_DmgType_HeavyAssault'] = "Heavy Assault";
 $w['R_DmgType_LAW'] = "LAW";
 $w['R_DmgType_Uzi'] = "Uzi";
 $w['R_DmgType_VM249'] = "VM249";
-$w['R_DmgType_SodaBomb'] = "Soda Bomb";
-$w['R_DmgType_UltralightHeliGun'] = "Ultra light HeliGun";
-$w['R_DmgType_SniperPrimary'] = "Sniper";
-$w['R_DmgType_OutOfCombat'] = "Out Of Combat";
-$w['UTDmgType_VehicleExplosion'] = "Vehicle Explosion";
-$w['R_DmgType_Cleaver'] = "Cleaver";
+$w['R_DmgType_357'] = ".357 Magnum";
+$w['R_DmgType_M249'] = "M249";
+$w['R_DmgType_Mp5'] = "MP5";
+$w['R_DmgType_M16'] = "M16";
+
+//Shotguns
+
+$w['R_DmgType_SawedOffShotgun'] = "Sawed Off Shotgun";
+$w['R_DmgType_Autoshotgun'] = "Auto-shotgun";
+
+
+//Sidearms
+
+//Rocket Launchers
+
 $w['R_DmgType_Stinger'] = "Stinger";
+$w['R_DmgType_RPG'] = "Rocket Propelled Grenade";
+
+//Equipment
+
+$w['R_DmgType_GKnife'] = "Knife";
 $w['R_DmgType_Sledgehammer'] = "Sledgehammer";
-$w['DmgType_Suicided'] = "Suicide";
+$w['R_DmgType_Cleaver'] = "Cleaver";
 $w['R_DmgType_Crossbow'] = "Crossbow";
 $w['R_DmgType_Bat'] = "Bat";
-$w['R_DmgType_Grenade_M79'] = "M79";
-$w['R_DmgType_P99'] = "P99";
-$w['DmgType_Fell'] = "Fell";
 $w['R_DmgType_Harpoon'] = "Harpoon";
+$w['R_DmgType_Grenade_M19'] = "M19 Grenade";
+$w['R_DmgType_TennisBallBomb'] = "Tennis Ball Bomb";
+$w['R_DmgType_SodaBomb'] = "Soda Bomb";
 $w['R_DmgType_M26Grenade'] = "M26 Grenade";
+$w['R_DmgType_RocketArtillery'] = "Rocket Artillery";
+
+//*********************
+// Vehicle names
+//*********************
+//Tanks
+
+
+//APCs
+
+$w['R_DmgType_StrykerCannon'] = "Stryker Cannon";
+
+
+//Helicopters
+$w['R_DmgType_HeliRocket'] = "Helicopter Rocket";
+$w['R_DmgType_HeliStinger'] = "Helicopter Stinger";
+$w['R_DmgType_UltralightHeliGun'] = "GyroCopter Machinegun";
+
+
+//Transport Vehicles
+
+
+
+//*********************
+// Other Weapon Names
+//*********************
+
+$w['UTDmgType_VehicleExplosion'] = "Vehicle Explosion";
+$w['R_DmgType_VehicleCollision'] = "Killed by Vehicle";
+$w['UTDmgType_VehicleCollision'] = "Crushed by Vehicle";
+$w['R_DmgType_OutOfCombat'] = "Out of Bounds";
+$w['DmgType_Suicided'] = "Suicides";
+$w['DmgType_Fell'] = "Fell";
+
 
 //*********************
 // Map names
 //*********************
-// Stock Homefront Maps
+//Stock
+$m['ctr_canyon'] = "Canyon";
+$m['ctr_canyon_day2'] = "Canyon Day 2";
+$m['ctr_icebreaker'] = "Ice Breaker";
+$m['ctr_oilrig'] = "Oilrig";
+$m['ctr_derelict'] = "Derelict";
+$m['CTR_Rooftop'] = "Rooftop";
+$m['ctr_bridge'] = "Bridge";
+$m['ctr_bridge_day2'] = "Bridge Day 2";
+$m['ctr_chasm'] = "Chasm";
+$m['ctr_liberty'] = "Liberty";
 $m['Thrust_IceBreaker'] = "IceBreaker";
 $m['Thrust_Bridge'] = "Bridge";
 $m['Thrust_Liberty'] = "Liberty";
 $m['Thrust_Rooftop'] = "Rooftop";
-$m['CTR_Bridge_Day2'] = "Bridge Day2";
 $m['Thrust_Oilrig'] = "Oilrig";
-$m['CTR_Chasm'] = "Chasm (CTR)";
-$m['CTR_Canyon_Day2'] = "Canyon Day2";
-$m['CTR_Derelict'] = "Derelict";
 $m['Thrust_Canyon'] = "Canyon";
-$m['CTR_Bridge'] = "Bridge";
-$m['CTR_Canyon'] = "Canyon (CTR)";
 $m['Thrust_Chasm'] = "Chasm";
+$m['Thrust_Derelict'] = "Derelict";
+
 
 $m['None'] = "-Unknown-";
 
 //*********************
 // Event names
 //*********************
+//$e[''] = "";
 
 //*********************
 // Bodypart names
 //*********************
 $b['none'] = $text["nonewop"];
 $b['body'] = $text["bodywop"];
+
 ?>
